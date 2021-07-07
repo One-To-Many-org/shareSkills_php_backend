81861c5779f6bf5d54885ec6594f2625f59aa8ba
Theophane <theophane.kouchoanou@ccsd.cnrs.fr>
2021-07-07 20:32:31 +0200

    service to add parse binary picture data to uploadFile and add it in request->files

---
+++ src/Service/FileUploader.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Service;
+
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class FileUploader
+{
+    protected $mapper;
+    public function __construct(MimeTypeMapperService $mapper){
+        $this->mapper=$mapper;
+    }
+   public function addFileTorequest($request,$fileBinary){
+           //pour avoir un nom unique ainsi si plusieurs processus utilisaiet le repertire la picture de l'un ne remplacera pas celle de l'autre
+           $fileName='tmp/picture'.uniqid ();
+
+           //on depose le fichier dans un dossier temporaire on determine son type et on renomme le fichier
+           // puis on crée un UploadFile qu'on rajoute à $_FILE de la requête avec test =true
+           if(false!==file_put_contents ($fileName,$fileBinary)){
+               $contentType=mime_content_type($fileName);
+               $ext=$this->mapper->guessExtension ($contentType);
+               rename($fileName,$fileName.'.'.$ext);
+               $fileName=$fileName.'.'.$ext;
+               //important de mettre test à true pour approuver que notre fichier est sure sinon cette fonction est utilisée https://www.php.net/manual/en/function.is-uploaded-file.php
+               $file['picture'] =new UploadedFile($fileName,$fileName,null,null,true);
+               $request->files->add ($file);
+           }
+
+   }
+}
